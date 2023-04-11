61ca61abe54002736acccd0b50b177493b2dd868
Scarma <113466976+Scarmatica@users.noreply.github.com>
2023-04-11 10:41:06 +0200

    Legacy kode

--- Main Controller m. lys.asm
+++ Main Controller m. lys.asm
@@ -1,7 +1,7 @@
 ;****************************************************************************
 ;*                           - Pic Projekt -                                *
 ;*                                                                          *
-;*  PROJEKT ANSV.:  	Nikolaj Høgh                                    	*      
+;*  PROJEKT ANSV.:  	Nikolaj HÃ¸gh                                    	*      
 ;*  NAVN:           	Main Controller										*
 ;*  PROCESSOR:      	PIC16F819                                   	    *
 ;*  CLOCK: 	    		Intern Osc - 31 kHz(Op til 8 MHZ m. intern osc.)	*
@@ -14,15 +14,15 @@
 
 ;*  PIN CONFIGURATION TIL HARDWAREN:
 
-;*  RA0 = Analog INPUT    ; PIN 17, Input fra forstærker
-;*  RA1 = Analog INPUT	  ; PIN 18, Input fra lysafhængig modstand
+;*  RA0 = Analog INPUT    ; PIN 17, Input fra forstÃ¦rker
+;*  RA1 = Analog INPUT	  ; PIN 18, Input fra lysafhÃ¦ngig modstand
 ;*  RA2 = Digital INPUT   ; PIN 1, Input fra radiosystem
 ;*  RA3 = N/A			  ; PIN 2, 
-;*  RA4 = Digital INPUT   ; PIN 3, Input fra pin 3/Q14 på 4060
+;*  RA4 = Digital INPUT   ; PIN 3, Input fra pin 3/Q14 pÃ¥ 4060
 ;*  RA5 = N/A			  ; PIN 4,
 ;*  RA6 = Digital OUTPUT  ; PIN 15 - Output til transistor til kraftig LED
-;*  RA7 = Digital OUTPUT  ; PIN 16 - Output til relæ for højtaler
-;*  Bemærk at følgende PIN-konfiguration er nødvendig, da AD-convertering skal bruges i programmet(DADAA)
+;*  RA7 = Digital OUTPUT  ; PIN 16 - Output til relÃ¦ for hÃ¸jtaler
+;*  BemÃ¦rk at fÃ¸lgende PIN-konfiguration er nÃ¸dvendig, da AD-convertering skal bruges i programmet(DADAA)
 
 
 
@@ -50,7 +50,7 @@ W		EQU		0		; 'direction' flag (target - hvor skal resultatet placeres)
 F		EQU		1		; 'direction' flag (target - hvor skal resultatet placeres)
 Carry	EQU		0		;
 
-TMR0	EQU 	0x0001	; Placerer TMR0 på adressen 01
+TMR0	EQU 	0x0001	; Placerer TMR0 pÃ¥ adressen 01
 						; TIMER0 er en 8-bit timer med 8-bit prescaler
 						; (TIMER1 er 16 bit)
 
@@ -63,7 +63,7 @@ ZEROBIT	EQU   	2   	;Zerobit er bit nr. 2 (i status-registret)
 ADCON0	EQU		0x001F	;A/D configurations register nr. 0  (ligger i bank 0) - Se datablad for PIC side 81
 ADCON1	EQU		0x009F	;A/D configurations register nr. 1  (ligger i bank 1) - Se datablad for PIC side 82
 
-ADRESH	EQU		0x001E	;De øverste 8 bit i AD-resultatet placeres i adresse 1E (De 8 mest betydende)
+ADRESH	EQU		0x001E	;De Ã¸verste 8 bit i AD-resultatet placeres i adresse 1E (De 8 mest betydende)
 ADRESL	EQU		0x009E	;De nederste 2 bit i AD-resultatet placeres i adresse 9E (De 2 mindst betydende)
 
 OPTION_R EQU 	0x0081	;
@@ -73,23 +73,23 @@ OSCCON	EQU		0x008F	;
 TRISA	EQU		0x0085	;Tristate Port A (in/out)
 TRISB	EQU		0x0086	;Tristate Port B (in/out)
 
-AN0		EQU		0		;Analog input AN0 er bit nr. 0 (på port A - dvs ben nr. 17)
+AN0		EQU		0		;Analog input AN0 er bit nr. 0 (pÃ¥ port A - dvs ben nr. 17)
 
-RA5		EQU		5		;RA5 er bit nr. 5 (på port A - ben 4)
+RA5		EQU		5		;RA5 er bit nr. 5 (pÃ¥ port A - ben 4)
 
-RB0		EQU		0		;RB0 er bit nr. 0 (på port B - ben 6)
-RB1		EQU		1		;RB1 er bit nr. 1 (på port B - ben 7)
-RB2		EQU		2		;RB2 er bit nr. 2 (på port B - ben 8)
-RB3		EQU		3		;RB3 er bit nr. 3 (på port B - ben 9)
-RB4		EQU		4		;RB4 er bit nr. 4 (på port B - ben 10)
-RB5		EQU		5		;RB5 er bit nr. 5 (på port B - ben 11) 
-RB6		EQU		6		;RB6 er bit nr. 6 (på port B - ben 12) 
-RB7		EQU		7		;RB7 er bit nr. 7 (på port B - ben 13)
+RB0		EQU		0		;RB0 er bit nr. 0 (pÃ¥ port B - ben 6)
+RB1		EQU		1		;RB1 er bit nr. 1 (pÃ¥ port B - ben 7)
+RB2		EQU		2		;RB2 er bit nr. 2 (pÃ¥ port B - ben 8)
+RB3		EQU		3		;RB3 er bit nr. 3 (pÃ¥ port B - ben 9)
+RB4		EQU		4		;RB4 er bit nr. 4 (pÃ¥ port B - ben 10)
+RB5		EQU		5		;RB5 er bit nr. 5 (pÃ¥ port B - ben 11) 
+RB6		EQU		6		;RB6 er bit nr. 6 (pÃ¥ port B - ben 12) 
+RB7		EQU		7		;RB7 er bit nr. 7 (pÃ¥ port B - ben 13)
 
-LYSRES	EQU		0x24	;Er denne nødvendig???????
+LYSRES	EQU		0x24	;Er denne nÃ¸dvendig???????
 LYDRES	EQU		0x25	;
 TELLER 	EQU 	0x26 	;
-CLKTEL	EQU		0x27	; Tælleren til hvor mange gange der er sket en forkert handling
+CLKTEL	EQU		0x27	; TÃ¦lleren til hvor mange gange der er sket en forkert handling
 
 
 ;****************************************************************************
@@ -99,9 +99,9 @@ CLKTEL	EQU		0x27	; Tælleren til hvor mange gange der er sket en forkert handling
 		LIST	P=PIC16F819			;angiver PIC-type
 		RADIX	hex					;Standard er hex
 		ORG		0x0000				;Programmet starter her, 
-									;org sætter origin til 0x0000
+									;org sÃ¦tter origin til 0x0000
 
-		GOTO	SETUP				;Gå til setup-delen, så porte defineres og alt bliver nulstillet
+		GOTO	SETUP				;GÃ¥ til setup-delen, sÃ¥ porte defineres og alt bliver nulstillet
 
 
 
@@ -124,7 +124,7 @@ CLKTEL	EQU		0x27	; Tælleren til hvor mange gange der er sket en forkert handling
 									;Lov Voltage Program. Disabled
 									;EE protect disabled
 									;Flash Program Write Protection Disabled
-									;CCP1 Mux på RB2 (ikke RB3)
+									;CCP1 Mux pÃ¥ RB2 (ikke RB3)
 									;Code Protection Disabled
 
 ;****************************************************************************
@@ -139,48 +139,48 @@ CLKTEL	EQU		0x27	; Tælleren til hvor mange gange der er sket en forkert handling
 
 SETUP	BCF		STATUS,6			;Dvs ikke bank 2 eller 3 - hhv 'b'10 og 'b'11 for RP1,RP0
 
-		BSF		STATUS,5			;Gå til Bank 1 (5. bit kaldes også RP0) 
-									; - operationen kaldes også "Bank Select"
+		BSF		STATUS,5			;GÃ¥ til Bank 1 (5. bit kaldes ogsÃ¥ RP0) 
+									; - operationen kaldes ogsÃ¥ "Bank Select"
 
-		MOVLW	B'00010111'			;Ift. AD-configurationen, så er følgende fordeling af  
-									;RA5 kan kun være input
-		MOVWF	TRISA				;Sæt in-out bitmønstret til tris-reg. port A (file 85H)
+		MOVLW	B'00010111'			;Ift. AD-configurationen, sÃ¥ er fÃ¸lgende fordeling af  
+									;RA5 kan kun vÃ¦re input
+		MOVWF	TRISA				;SÃ¦t in-out bitmÃ¸nstret til tris-reg. port A (file 85H)
 
 
 		MOVLW	B'00000000'			;RB0-RB7 (Hele portB) er output
 		MOVWF	TRISB				;Placer informationen i tris-reg. port B (file 86H)
 
 
-		MOVLW	B'00000100'			;Fortæller at AD-configurationen følger DADAA input (se side 170 i bogen
-									;"PIC in practice" - eller på side 82 i datablad for 16F818/819)
+		MOVLW	B'00000100'			;FortÃ¦ller at AD-configurationen fÃ¸lger DADAA input (se side 170 i bogen
+									;"PIC in practice" - eller pÃ¥ side 82 i datablad for 16F818/819)
 									; Se RIGISTER 11-2 i databladet
 		MOVWF	ADCON1				;Informationen placeres i AD control register nr. 1
 
-		MOVLW	B'01000100'			;Fortæller intern clock skal være 1 MHz (Op til 8 MHz), samt at PIC venter med at starte koden indtil frekvensen er stabil
+		MOVLW	B'01000100'			;FortÃ¦ller intern clock skal vÃ¦re 1 MHz (Op til 8 MHz), samt at PIC venter med at starte koden indtil frekvensen er stabil
 									;(Se side 38 i datablad) ,  bit  IOFS=0 
-		MOVWF	OSCCON				;OSCCON ligger på adressen 0x8F - altså i bank 1
+		MOVWF	OSCCON				;OSCCON ligger pÃ¥ adressen 0x8F - altsÃ¥ i bank 1
 
 		
-		MOVLW	B'00000111'			;PreScaler er 1:256 -  Dvs. timerens tællehastighed er rimelig langsom !!!
+		MOVLW	B'00000111'			;PreScaler er 1:256 -  Dvs. timerens tÃ¦llehastighed er rimelig langsom !!!
 									; (Bit 7 og 6 SKAL LIGE CHECKES m.h.p. INTERRUPT-KONTROL )
-		MOVWF	OPTION_R			;FortÃ¦l om prescaler-tallet til option registret
+		MOVWF	OPTION_R			;FortÃƒÂ¦l om prescaler-tallet til option registret
 
 		BCF		STATUS,5			;Vi hopper tilbage til bank 0. 
-									;(Vi er nemlig færdige med at snakke med de filer, som ligger i bank 1)
+									;(Vi er nemlig fÃ¦rdige med at snakke med de filer, som ligger i bank 1)
 
 		CLRF	PORTA				;Nulstil portA
 		CLRF 	PORTB				;Nulstil portB
 	
-	;	CALL	HALLØJ				;Gå til Halløj
+	;	CALL	HALLÃ˜J				;GÃ¥ til HallÃ¸j
 
 		BCF 	PORTA,7
 		BCF 	PORTA,6
 
 		
-		GOTO	MAIN				; Gå til main
+		GOTO	MAIN				; GÃ¥ til main
 
 
-; SLUT PÅ SETUP. 
+; SLUT PÃ… SETUP. 
 
 
 ;****************************************************************************
@@ -216,7 +216,7 @@ LOOP5           MOVFW   TMR0
                 RETURN
 
 
-HALLØJ			MOVLW	b'11000000'  	; Halløj får LED til at lyse og relæet aktiveres
+HALLÃ˜J			MOVLW	b'11000000'  	; HallÃ¸j fÃ¥r LED til at lyse og relÃ¦et aktiveres
 				MOVWF	PORTA
 				CALL	PAUSE
 				CLRF	PORTA
@@ -225,22 +225,22 @@ HALLØJ			MOVLW	b'11000000'  	; Halløj får LED til at lyse og relæet aktiveres
 
 
 LYDBEHANDLING	CALL 	VENT			; Lader lyden lyde VENTs tid
-				BSF		PORTA,7 		; Tænd for strømmen/tænd relæet
-				CALL 	VENT			; Vent i et stykke tid indtil lyden er dødt hen
-				BCF		PORTA,7			; Sluk for strømmen/sluk relæet - lyden løber gennem nu
-				CALL	TÆL1OP			; Læg 1 til displayet der viser antal gange systemet er blevet aktiveret
+				BSF		PORTA,7 		; TÃ¦nd for strÃ¸mmen/tÃ¦nd relÃ¦et
+				CALL 	VENT			; Vent i et stykke tid indtil lyden er dÃ¸dt hen
+				BCF		PORTA,7			; Sluk for strÃ¸mmen/sluk relÃ¦et - lyden lÃ¸ber gennem nu
+				CALL	TÃ†L1OP			; LÃ¦g 1 til displayet der viser antal gange systemet er blevet aktiveret
 				RETURN
 
-LYSBEHANDLING	BSF		PORTA,6			; Tænd for bit 6, som tænder for den kraftige LED
-				CALL	VENT			; Vent en smule tid, så lyset bemærkes
+LYSBEHANDLING	BSF		PORTA,6			; TÃ¦nd for bit 6, som tÃ¦nder for den kraftige LED
+				CALL	VENT			; Vent en smule tid, sÃ¥ lyset bemÃ¦rkes
 				BCF		PORTA,6			; Sluk for bit 6 igen
-				CALL	TÆL1OP
+				CALL	TÃ†L1OP
 				RETURN
 
-RADIOBEHANDLING	BSF		PORTA,7			; Tænd for relæet
+RADIOBEHANDLING	BSF		PORTA,7			; TÃ¦nd for relÃ¦et
 RADIOLOOP		BTFSC	PORTA,2
 				GOTO	RADIOLOOP
-				BCF		PORTA,7			; Sluk for relæet
+				BCF		PORTA,7			; Sluk for relÃ¦et
 				RETURN					
 
 
@@ -249,20 +249,20 @@ RADIOLOOP		BTFSC	PORTA,2
 PAUSE_800_US	CLRF	TMR0 			; Timer0 nulstilles
 LOOP1 			MOVFW	TMR0 			; Flyt indholdet af TMR0 til W-registret
 				SUBLW	d'47'
-				BTFSS	STATUS,ZEROBIT 	; Tjek om det gav nul. Hvis ja: hop ud af løkken
-				GOTO	LOOP1			; Hvis nej: bliv i løkken (gå til LOOP1-label)
+				BTFSS	STATUS,ZEROBIT 	; Tjek om det gav nul. Hvis ja: hop ud af lÃ¸kken
+				GOTO	LOOP1			; Hvis nej: bliv i lÃ¸kken (gÃ¥ til LOOP1-label)
 				RETURN 					; Forlad subrutinen
 				
 
 CHECKLYD	
 				MOVLW	B'00000000'		;Configurer ADCON og sluk denne
 				MOVWF	ADCON0			;
-				BSF		ADCON0,0		;Tænd ADCON - ADON = 1
+				BSF		ADCON0,0		;TÃ¦nd ADCON - ADON = 1
 				BSF 	ADCON0,2		;Starter adconverteringen og venter til denne er lav igen
 LOOP_AD_LYD		BTFSC	ADCON0,2	
 				GOTO	LOOP_AD_LYD
 				MOVFW 	ADRESH	 		;Move Analogue result into W
-				SUBLW 	d'170' 			;Do 122 – ADRES, i.e. 122-W
+				SUBLW 	d'170' 			;Do 122 Â– ADRES, i.e. 122-W
 				BTFSC 	STATUS,Carry 	;Check the carry bit. Clear if ADRES>122 i.e. ve
 				RETURN			 		;Routine to turn off LED
 				CALL 	LYDBEHANDLING	;Routine to turn on LED
@@ -272,48 +272,48 @@ LOOP_AD_LYD		BTFSC	ADCON0,2
 CHECKLYS		CLRF	ADRESH
 				MOVLW	B'00001000'		;Configurer ADCON og sluk denne
 				MOVWF	ADCON0			;
-				BSF		ADCON0,0		;Tænd ADCON - ADON = 1
+				BSF		ADCON0,0		;TÃ¦nd ADCON - ADON = 1
 				BSF 	ADCON0,2		;Starter adconverteringen og venter til denne er lav igen
 LOOP_AD_LYS		BTFSC	ADCON0,2	
 				GOTO	LOOP_AD_LYS	
 				MOVFW 	ADRESH 			;Flyt Analog resultat ind i W
-				SUBLW 	d'135' 			;Laver 200 – ADRES, i.e. 200-W
+				SUBLW 	d'135' 			;Laver 200 Â– ADRES, i.e. 200-W
 				BTFSC 	STATUS,Carry 	;Check the carry bit. Clear if ADRES>122
 				RETURN			 		;Routine to turn off LED
 				CALL 	LYSBEHANDLING	;Routine to turn on LED
 				RETURN
 
-TÆL1OP			MOVFW	PORTB			;Flytter 9 til w-registret
-				CALL	CLR2NIB			;Tømmmer nibble 2 
-				SUBLW	d'9'			;Trækker 10 fra nibble 1
-				BTFSC 	STATUS,ZEROBIT	;Tjekker om nibble 1 var 10(om den er clear), hvis den var, så spring over næste kommando
-				GOTO	TÆL10OP			;Går til subrutinen som lægger 1 til nibble 2
-				ADDWF	W,d'1'			;Hvis zerobit er clear(nibble 1 var ikke 10), så læg 1 til w-registret
-HJEM_TÆL		MOVWF	PORTB			;Flyt w-registret til port b
+TÃ†L1OP			MOVFW	PORTB			;Flytter 9 til w-registret
+				CALL	CLR2NIB			;TÃ¸mmmer nibble 2 
+				SUBLW	d'9'			;TrÃ¦kker 10 fra nibble 1
+				BTFSC 	STATUS,ZEROBIT	;Tjekker om nibble 1 var 10(om den er clear), hvis den var, sÃ¥ spring over nÃ¦ste kommando
+				GOTO	TÃ†L10OP			;GÃ¥r til subrutinen som lÃ¦gger 1 til nibble 2
+				ADDWF	W,d'1'			;Hvis zerobit er clear(nibble 1 var ikke 10), sÃ¥ lÃ¦g 1 til w-registret
+HJEM_TÃ†L		MOVWF	PORTB			;Flyt w-registret til port b
 				RETURN
 
-TÆL10OP			MOVFW	PORTB			;Flytter data fra port b til w-registret
-				ADDWF	W,d'6'			;Lægger 6 til sådan at vi omgår hexadecimal notation
-				GOTO	HJEM_TÆL		;Bruges til at hoppe over kommandoen er lægger 1 til
+TÃ†L10OP			MOVFW	PORTB			;Flytter data fra port b til w-registret
+				ADDWF	W,d'6'			;LÃ¦gger 6 til sÃ¥dan at vi omgÃ¥r hexadecimal notation
+				GOTO	HJEM_TÃ†L		;Bruges til at hoppe over kommandoen er lÃ¦gger 1 til
 
 
 
 
-SET1NIB			BSF		W,0		;Subrutine til at sætte nibble 1 til 1111
-				BSF		W,1		; Er denne nødvendig
+SET1NIB			BSF		W,0		;Subrutine til at sÃ¦tte nibble 1 til 1111
+				BSF		W,1		; Er denne nÃ¸dvendig
 				BSF		W,2
 				BSF		W,3
 				RETURN
 
 
-CLR1NIB			BCF		W,0		;Subrutine til at sætte nibble 1 til 0000
+CLR1NIB			BCF		W,0		;Subrutine til at sÃ¦tte nibble 1 til 0000
 				BCF		W,1		
 				BCF		W,2
 				BCF		W,3
 				RETURN
 
-CLR2NIB			BCF		W,4		;Subrutine til at sætte nibble 1 til 0000
-				BCF		W,5		;Er denne nødvendig
+CLR2NIB			BCF		W,4		;Subrutine til at sÃ¦tte nibble 1 til 0000
+				BCF		W,5		;Er denne nÃ¸dvendig
 				BCF		W,6
 				BCF		W,7
 				RETURN
@@ -330,8 +330,8 @@ CLR2NIB			BCF		W,4		;Subrutine til at sætte nibble 1 til 0000
 
 MAIN			CALL 	CHECKLYD
 				CALL 	CHECKLYS
-				BTFSC 	PORTA, 2		; Tjek om der er strøm fra radiosensoren
-				CALL 	RADIOBEHANDLING	; Gå til radiobehandling
+				BTFSC 	PORTA, 2		; Tjek om der er strÃ¸m fra radiosensoren
+				CALL 	RADIOBEHANDLING	; GÃ¥ til radiobehandling
 				CALL 	PAUSE_800_US
 				GOTO 	MAIN
 
